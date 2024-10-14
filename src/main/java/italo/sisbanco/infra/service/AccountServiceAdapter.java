af584753d541d04b4a633286f0ac08a62d51ee6b
Italo Herbert Siqueira Gabriel <italoherbert@outlook.com>
2024-10-14 16:37:52 -0300

    Após retirado um System.out.println desnecessario

--- src/main/java/italo/sisbanco/infra/service/AccountServiceAdapter.java
+++ src/main/java/italo/sisbanco/infra/service/AccountServiceAdapter.java
@@ -25,8 +25,6 @@ public class AccountServiceAdapter implements AccountServicePort {
     public Account save(Account account) {
         AccountEntity entity = accountMapper.map( account );
         AccountEntity regEntity = accountRepository.save( entity );
-        System.out.println( "XXXXXXXXXXXXX "+entity+"     "+accountMapper.map( regEntity ) );
-
         return accountMapper.map( regEntity );
     }
 
